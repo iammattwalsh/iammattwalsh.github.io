30157674415fb5b07098af324112f9ff74efb69a
MattDWalsh <matthew.david.walsh@gmail.com>
2022-09-26 15:06:43 -0700

    added project thumbnails with Vue.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,53 @@
+// import jsonTest from 'projectassets.json'
+// console.log(jsonTest)
+// export default {
+//     data() {
+//         return {
+//             myJson: json
+//         }
+//     }
+// }
+// projectsJson = fetch('projectassets.json').json()
+// fetch('projectassets.json').then(res => projectsJson = res.json())
 // generate app
+
+// import jsonObject from 'projectassets.json'
+
+
+
 const { createApp } = Vue
 createApp({
     data () {
         return {
-            // set up variables
-            test: {
-                test: 'hello'
-            },
-            test2: 'world'
+            projectAssets: [],
         }
     },
+    created() {
+        axios ({
+            method: 'get',
+            url: 'projectassets.json',
+        }).then(res => {
+            let resData = Object.values(res.data)
+            Object.values(res.data).forEach((_, i) => {
+                if (i % 2 == 0) {
+                    this.projectAssets.push([resData[i], resData[i + 1]])
+                }
+            })
+            // console.log(resData)
+            console.log(this.projectAssets)
+            });
+        
+    },
     methods: {
+        test() {
+            console.log('boop!')
+        }
     }
 }).mount('#app')
+
+
+// add overflow:hidden; to main on popup, remove on close & nav interaction
+// ^^^ consider if this is good for UX
+
+// add close button and prev/next buttons (with project names)
+
